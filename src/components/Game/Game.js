4bb6f827a92a3fa0a66f6aa443b007271b0c3d85
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-04-02 19:37:54 +0100

    feat: display starter guesses as grid

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,8 +1,9 @@
 import React from "react";
 
+import { ALLOWED_GUESS_LENGTH, NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import { WORDS } from "../../data";
 import "../../styles.css";
-import { sample } from "../../utils";
+import { range, sample } from "../../utils";
 import GuessInput from "../GuessInput/GuessInput";
 import GuessResults from "../GuessResults/GuessResults";
 
@@ -12,7 +13,21 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  const [guessesList, setGuessesList] = React.useState([]);
+  const [guessesList, setGuessesList] = React.useState(
+    range(0, NUM_OF_GUESSES_ALLOWED).map(() => {
+      return {
+        guessId: crypto.randomUUID(),
+        guess: range(0, ALLOWED_GUESS_LENGTH).map(() => {
+          return {
+            letterId: crypto.randomUUID(),
+            letter: "",
+          };
+        }),
+      };
+    })
+  );
+
+  console.log({ guessesList });
 
   return (
     <>
