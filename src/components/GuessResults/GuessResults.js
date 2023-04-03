76cdb2d666bd477e74d0f489b9c1021e31000f1e
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-04-03 11:02:59 +0100

    feat: style letter styles from status

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -7,10 +7,10 @@ function GuessResults({ guessesList }) {
     <div className="guess-results">
       {guessesList.map(({ guessId, guess }) => {
         return (
-          <p key={guessId} className="guess">
-            {guess.map(({ letterId, letter }) => {
+          <p key={guessId} className={`guess`}>
+            {guess.map(({ letterId, letter, status }) => {
               return (
-                <span key={letterId} className="cell">
+                <span key={letterId} className={`cell ${status}`}>
                   {letter}
                 </span>
               );
