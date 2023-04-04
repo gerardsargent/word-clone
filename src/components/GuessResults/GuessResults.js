50d98445c2130b71ca157b289c2ee65307a0d288
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-04-04 16:05:27 +0100

    feat: animate correct letter guesses

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -5,14 +5,14 @@ function GuessResults({ guessesList, turnNumber }) {
     <div className="guess-results">
       {guessesList.map(({ guessId, guess }, index) => {
         const shouldAnimate = index === turnNumber - 1;
-        console.log({ index, turnNumber });
+
         return (
           <p key={guessId} className="guess">
             {guess.map(({ letterId, letter, status }) => {
               return (
                 <span
                   key={letterId}
-                  className={`cell ${status} ${shouldAnimate ? "grow" : ""}`}
+                  className={`cell ${status} ${shouldAnimate ? `grow` : ""}`}
                 >
                   {letter}
                 </span>
